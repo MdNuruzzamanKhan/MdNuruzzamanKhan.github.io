d83703d2b545bad1d6da7f8ba79921fd19ab1b88
Nuruzzaman <146788800+MdNuruzzamanKhan@users.noreply.github.com>
2023-11-15 21:35:30 +0600

    Update function.js

--- function.js
+++ function.js
@@ -1,27 +1,2 @@
- const  p=Math.floor(1000+(Math.random()* 8999));
-function pin(){
-var b = document.getElementById("fn");
- let c=fn.value;
- alert(`Hey ${c}, Welcome to my first website!!!`);
-
-
-const div=document.getElementById("frm");
-div.innerHTML =`Your PIN=${p}
-Click the button 'CODE' in topbar and enter this pin...` ;
-}
-function bio(){
-  var a = document.getElementById("ame");
-  a.style.display="block";}
-function pw(){
-  var pw = document.getElementById("pw");
-  let pw1 = pw.value;
-  if((pw1 == p)||(pw1=="@admin")){
-  var op = document.getElementById("submit");
-  op.href ='https://mdnuruzzamankhan.github.io/codes/code.html'}
-  else{var opp = document.getElementById("cg");
-  opp.innerHTML="Incorrect pin, try again...";}
-}
-function inf(){
-  var d = document.getElementById("frm");
-  d.style.display="block";
-}
+ document.getElementById("new").href=
+"https://mdnuruzzamankhan.github.io/codes/code6.html";
