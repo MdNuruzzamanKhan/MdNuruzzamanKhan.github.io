f21518b1b7073f7dc724f61e838b6edf82a66ee8
Nuruzzaman <146788800+MdNuruzzamanKhan@users.noreply.github.com>
2023-11-22 20:11:05 +0600

    Update function.js

--- function.js
+++ function.js
@@ -1,2 +1,2 @@
  document.getElementById("new").href=
-"https://github.com/MdNuruzzamanKhan/MdNuruzzamanKhan.github.io/blob/main/raws/Set.html";
+"https://github.com/MdNuruzzamanKhan/MdNuruzzamanKhan.github.io/blob/main/raws/set.py";
