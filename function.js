76b100fa0395061e2f805088bd451dbed75d13f9
Nuruzzaman <146788800+MdNuruzzamanKhan@users.noreply.github.com>
2023-11-26 07:06:54 +0600

    Update function.js

--- function.js
+++ function.js
@@ -1,2 +1,3 @@
  document.getElementById("new").href=
-"https://github.com/MdNuruzzamanKhan/MdNuruzzamanKhan.github.io/blob/main/raws/set.py";
+//"https://github.com/MdNuruzzamanKhan/MdNuruzzamanKhan.github.io/blob/main/raws/set.py";
+"https://mdnuruzzamankhan.github.io/codes/pass.html"
